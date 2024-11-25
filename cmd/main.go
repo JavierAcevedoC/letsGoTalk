39c7138c6d8309866b2255de05016ef5e4eaee23
Javier Acevedo C <javier.iacevedoc@gmail.com>
2024-11-24 21:08:23 -0300

    feat(main): add main

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"letsGOTalk/internal/chat"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func serveWs(hub *chat.Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade error:", err)
+		return
+	}
+	client := &chat.Client{Hub: hub, Conn: conn, Send: make(chan []byte, 256)}
+	hub.Register <- client
+
+	go client.WritePump()
+	go client.ReadPump()
+}
+
+func main() {
+	hub := chat.NewHub()
+	go hub.Run()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+
+	log.Println("Server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe error:", err)
+	}
+}
